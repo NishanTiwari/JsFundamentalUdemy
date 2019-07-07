72eeb41edeac762abbb374dd74ad911e8938ac24
coolnishan1994@gmail.com <coolnishan1994@gmail.com>
2019-07-07 13:34:20 +0300

    js basic ternary.js

--- jsBasic/ternary.js
+++ jsBasic/ternary.js
@@ -16,4 +16,29 @@ console.log(drink);
 
 
 
-// 
+// switch case statement
+var day = "sunday"
+switch (day) {
+    case 0:
+        day = "Sunday is boaring";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+}
+
+console.log(day)
