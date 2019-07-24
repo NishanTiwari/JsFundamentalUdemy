b5c2a4640493261070e3f6ea20298f42a402fb1e
coolnishan1994@gmail.com <coolnishan1994@gmail.com>
2019-07-25 00:16:16 +0300

    new update

--- jsBasic/function.js
+++ jsBasic/function.js
@@ -1,38 +1,57 @@
 
-// function calculateAge (birthyear) {
-//     return 2019 - birthyear;
-
-// }
-// var ageJohn =calculateAge(1993);
-// console.log(ageJohn);
-
 // var count = 0;
 
 // function cc(card) {
-//   // Only change code below this line
+// debugger
+//     count = 0 //even 1  odd -1
+//     function recon(num) {
+//         if (num % 2 === 0) {
+//             count += 1
+//         } else if (num % 2 !== 0) {
+//             count -= 1
+//         } else {
+//             return
+//         }
+//     }
+
+//     recon(2); recon(4); recon(6)
+//     console.log('count', count);
+
+//     //  calculate Birth Year
+
+//     function calculateAge(birthYear) {
+//         return 2018 - birthYear;
+//     }
+
+//     function yearUntilRetirement(year, firstName) {
+//         var age = calculateAge(year)
+//         var retirement = 65 - age;
+//         if (retirement > 0) {
+//             console.log(firstName + "retires in " + retirement + "years.");
+//         }
+//         else {
+//             console.log(firstName + "is already retired")
+//         }
+//     }
+
+//     yearUntilRetirement(1993, "John");
+//     yearUntilRetirement(1948, "MANISH");
+//     yearUntilRetirement(1967, "BISHAL");
+//     yearUntilRetirement(1996, "Kami");
 
 
-//   return "Change Me";
-//   // Only change code above this line
-// }
+// function using swich statement¨
 
-// // Add/remove calls to test your function.
-// // Note: Only the last will display
-// cc(2); cc(3); cc(7); cc('K'); cc('A');
-
-// recon od or even
-
-count = 0 //even 1  odd -1
-function recon(num) {
-    if (num % 2 === 0) {
-        count += 1
-    } else if (num % 2 !== 0) {
-        count -= 1
-    } else {
-
-        return
+var whatDoYouDO = function (job, firstName) {
+    switch (job) {
+        case "teacher":
+            return firstName + " teaches code to children"
+        case "driver":
+            return firstName + " drive the cab "
+        case "designer":
+            return firstName + " design beautiful website"
+        default:
+            return "looking for job"
     }
 }
-
-recon(2); recon(4); recon(6)
-console.log('count', count)
+console.log(whatDoYouDO("teacher", "ram"));
---
+++ jsUnderTheHood/jsHosting.js
@@ -0,0 +1,37 @@
+// lecture Hoasting
+function calculateAge(year) {
+    console.log(2019 - year);
+}
+calculateAge(1993);
+
+console.log("hello World")
+
+
+// 
+// First scoping example
+var a = 'Hello!';
+first();
+function first() {
+    var b = 'Hi!';
+    second();
+    function second() {
+        var c = 'Hey!';
+        console.log(a + b + c);
+    }
+}
+// Example to show the differece between execution stack and scope chain
+var a = 'Hello!';
+first();
+function first() {
+    var b = 'Hi!';
+    second();
+    function second() {
+        var c = 'Hey!';
+        third()
+    }
+}
+function third() {
+    var d = 'John';
+    //console.log(c);
+    console.log(a + d);
+}
